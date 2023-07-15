6f4aaca8589a6d1d333e689e505e9788496c6733
NoBody <113786996+amppmann@users.noreply.github.com>
2023-07-16 00:18:03 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import {createRoot} from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import App from "./Components/App.jsx";
+
+const app = ReactDOM.createRoot(document.getElementById("app"));
+
+app.render(<App />);
